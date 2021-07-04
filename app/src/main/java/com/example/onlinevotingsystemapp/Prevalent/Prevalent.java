621381e5ea1fae0088deaf28ad7ee654bad593d0
Ameena7299 <mcsf19a026@pucit.edu.pk>
2021-07-04 16:34:04 -0700

    Added UserUpdatePassword file and firebase code of login and home page

---
+++ app/src/main/java/com/example/onlinevotingsystemapp/Prevalent/Prevalent.java
@@ -0,0 +1,8 @@
+package com.example.onlinevotingsystemapp.Prevalent;
+
+import com.example.onlinevotingsystemapp.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+}
